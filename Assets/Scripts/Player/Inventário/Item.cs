8ee04a141affa9c0cb8ae2efd5634efa8373134e
erickterra <erickgarciaterra@gmail.com>
2019-03-27 16:40:05 -0300

    Organização de pastas e Database implementado
    Implementado o database simples.
    Implementadas pastas para organização do projeto.

---
+++ Assets/Scripts/Player/Inventário/Item.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class Item 
+{
+    //a classe Item é uma classe abstrata, ou seja, não herda do Monobehavior. 
+    //O que quer dizer que ela não tem código, ela só serve pra segurar propriedades.
+    //Ela é o "modelo" do que um item deve ter, as instancias vão herdar dessa classe.
+
+    public string itemName; //o nome do item
+    public int itemID; //ID lógico do item
+    public string itemDesc; //Uma breve descrição do item
+    public Sprite itemIcon; // o icone 2D que vai ser exibido no inventário
+    public GameObject itemModel; //o prefab do item
+
+    public Item(string name, int id, string desc)
+    {
+        //Esse é o construtor da classe, serve para criar novos itens (pelo database)
+        itemName = name;
+        itemID = id;
+        itemDesc = desc;
+        itemIcon = Resources.Load<Sprite>("Icons/" + name);
+        itemModel = Resources.Load<GameObject>("Models/" + name);
+        //os campos de icone e de modelo 3D tem um caso especial, eles são carregados automaticamente, desde que estejam nas pastas certas
+        //e os nomes estejam IDENTICOS ao nome dado ao item;
+    }
+
+    public Item()
+    {
+        //um construtor de item vazio. Ele serve para representar um slot sem nada.
+        itemID = -1;
+    }
+    
+    
+}
---
+++ Assets/Scripts/Player/Inventário/ItemDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    public List<Item> itens = new List<Item>(); 
+    // Uma lista do tipo Items (clase abstrata)
+
+    private void Start() 
+    {
+        //itens.Add(new Item("", itens.Count, "")); //Construtor padrão da classe Item
+        itens.Add(new Item("Livro", itens.Count, "Um livro"));
+
+    }
+}
