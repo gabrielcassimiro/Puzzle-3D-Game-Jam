7448bbb1d8999f7595c0f7f01f069cf592712ff4
Gabriel Cassimiro <gdinizcassimiro@gmail.com>
2019-03-27 13:51:59 -0300

    Movimentação do Player Concluido
    Movimentação do player completa.

---
+++ Assets/Scripts/Player/Movement/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private string horizontalInputName, verticalInputName;
+
+    [SerializeField] private float movementSpeed;
+
+    private CharacterController charController;
+
+    private void Awake() {
+        charController = GetComponent<CharacterController>();
+    }
+
+    private void Update() {
+        playerMovement();
+    }
+
+    private void playerMovement(){
+        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+        float verInput = Input.GetAxis(verticalInputName) * movementSpeed;
+
+        Vector3 forwardMove = transform.forward * verInput;
+        Vector3 rightMove = transform.right * horizInput;
+
+        charController.SimpleMove(forwardMove + rightMove);
+    }
+}
