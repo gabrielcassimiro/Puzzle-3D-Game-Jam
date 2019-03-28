d98cde4c1f3b932948bf4400398422007fef4de3
erickterra <erickgarciaterra@gmail.com>
2019-03-28 14:10:20 -0300

    Instanciar do Database

---
+++ Assets/Scripts/Ambiente/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public int id; // o id do item a ser instanciado
+    private ItemDatabase database;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        database = GameObject.FindGameObjectWithTag("Database").GetComponent<ItemDatabase>();
+        Item item = database.GetItemByID(id); //Pega o item no database pelo ID;
+        Instantiate(item.itemModel, this.transform.position, this.transform.rotation); //Pega o modelo salvo no database e instancia
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
