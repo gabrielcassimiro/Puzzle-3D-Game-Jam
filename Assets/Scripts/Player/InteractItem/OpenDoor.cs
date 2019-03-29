a14afebdcaba2daa16f4c1a9c124492515532107
Heric Felipe de Freitas <0040961713035@americana.fatec.br>
2019-03-29 19:58:15 -0300

    door

---
+++ Assets/Scripts/Player/InteractItem/OpenDoor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+
+    private Renderer book;
+
+    private void Start()
+    {
+        book = GetComponent<MeshRenderer>();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Livro"))
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                book.enabled = true;
+                //bool da porta setado pra true E fazer a animação rodar
+            }
+        }   
+    }
+}
