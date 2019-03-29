bfddc8e5df40b0b583cf1e7e83831cd154bd2425
Heric Felipe de Freitas <35154816+hefefe@users.noreply.github.com>
2019-03-29 12:40:29 -0300

    alavanca
    ESSA MERDA TA DANDO MERDA

---
+++ Assets/Scripts/Player/InteractItem/ActionLever.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionLever : MonoBehaviour
+{
+    RaycastHit hit;
+    Rigidbody rb;
+    Vector3 movimento;
+
+    public bool canClick;
+
+    void Start () {
+        rb = GetComponent<Rigidbody> ();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Item"))
+        {
+            canClick = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Item"))
+        {
+            canClick = false;
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50) && Input.GetMouseButton(0))
+        {
+            Vector3 playerToMouse = hit.point - transform.position;
+            playerToMouse.y = 0;
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            rb.MoveRotation(newRotation);
+        }
+
+    }
+
+}
