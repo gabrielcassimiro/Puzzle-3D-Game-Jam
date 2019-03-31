4948bf81fa64b2c798bb4385932817f647bb0df2
dantasleonardo <47763070+dantasleonardo@users.noreply.github.com>
2019-03-31 13:39:08 -0300

    Pause

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+
+    bool isPause;
+
+    void PauseC()
+    {
+        Time.timeScale = 0;
+    }
+
+    void UnPause()
+    {
+        Time.timeScale = 1;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            isPause = !isPause;
+
+            if (isPause)
+            {
+                PauseC();
+            }
+            else
+            {
+                UnPause();
+            }
+        }
+    }
+}
